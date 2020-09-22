32b66f014218467884e410e56f3172cfec30dc69
Jose-Hash-Dev <66011783+Jose-Hash-Dev@users.noreply.github.com>
2020-09-22 16:12:46 +0300

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -5,7 +5,7 @@ input_path = cv2.VideoCapture('videos/example_01.mp4')
 def saveFrame(startSec, endSec):
     input_path.set(cv2.CAP_PROP_POS_MSEC,startSec*1000)
     hasFrames, image = input_path.read()    
-    output_path = (r'C:\Users\user\Desktop\Video to frame\images')
+    output_path = (r'C:\Users\user_name\Desktop\Video to frame\images')
     if hasFrames:
         cv2.imwrite(output_path + "\\frame%d.jpg" % frame_name_count, image)
     return hasFrames
